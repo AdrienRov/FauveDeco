59f5fd6a23dacf18f8669bf450d2ef8189fd7c90
LEGRAND Hugo <legrand.hugo1732@gmail.com>
2023-12-13 16:57:40 +0100

    Serialization des objets et tout

--- server/src/Controller/ProductController.php
+++ server/src/Controller/ProductController.php
@@ -3,26 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/products', name: 'app_products', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): JsonResponse
+    private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
     {
-        $products = $entityManager->getRepository(Product::class)->findAll();
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+    }
 
-        $arr = [];
+    #[Route('/products', name: 'app_products', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $productArray = array();
+
         foreach ($products as $product) {
-            $arr[] = $this->serializeProduct($product);
+            $productArray[] = $this->serializeProduct($product);
         }
-
-        return $this->json($arr);
+        return $this->json($productArray);
     }
 
     #[Route('/product/{id}', name: 'app_product', methods: ['GET'])]
@@ -32,7 +42,7 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product', name: 'app_product_create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
+    public function create(Request $request, ValidatorInterface $validator): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $product = $this->createProductFromRequest($data);
@@ -43,23 +53,23 @@ class ProductController extends AbstractController
             return $this->json(['status' => false, 'error' => (string) $errors], 400);
         }
 
-        $entityManager->persist($product);
-        $entityManager->flush();
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
 
         return $this->json(['status' => true, 'id' => $product->getId()]);
     }
 
     #[Route('/product/{id}', name: 'app_product_delete', methods: ['DELETE'])]
-    public function delete(Product $product, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(Product $product): JsonResponse
     {
-        $entityManager->remove($product);
-        $entityManager->flush();
+        $this->entityManager->remove($product);
+        $this->entityManager->flush();
 
         return $this->json(['status' => true]);
     }
 
     #[Route('/product/{id}', name: 'app_product_update', methods: ['PATCH'])]
-    public function update(Request $request, Product $product, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
+    public function update(Request $request, Product $product, ValidatorInterface $validator): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $product = $this->updateProductFromRequest($product, $data);
@@ -70,8 +80,8 @@ class ProductController extends AbstractController
             return $this->json(['status' => false, 'error' => (string) $errors], 400);
         }
 
-        $entityManager->persist($product);
-        $entityManager->flush();
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
 
         return $this->json(['status' => true]);
     }
@@ -79,14 +89,19 @@ class ProductController extends AbstractController
 
     private function serializeProduct(Product $product): array
     {
+        $category = null;
+        if ($product->getCategory() != null) {
+            $category = $this->entityManager->getRepository(Category::class)->find($product->getCategory()->getId());
+            $category = $category->serialize($category);
+        }
+
         return [
             'id' => $product->getId(),
             'name' => $product->getName(),
             'price' => $product->getPrice(),
             'description' => $product->getDescription(),
             'quantity' => $product->getQuantity(),
-            'category' => $product->getCategory() ? $product->getCategory()->getId() : null,
-
+            'category' => $category
         ];
     }
 
@@ -97,14 +112,14 @@ class ProductController extends AbstractController
         $price = $data['price'] ?? null;
         $description = $data['description'] ?? null;
         $quantity = $data['quantity'] ?? null;
+        $category = $data['category'] ?? null;
 
         $product = new Product();
         $product->setName($name);
         $product->setPrice($price);
         $product->setDescription($description);
         $product->setQuantity($quantity);
-
-
+        $product->setCategory($this->entityManager->getRepository(Category::class)->find($category)->getId());
 
         return $product;
     }
@@ -116,6 +131,7 @@ class ProductController extends AbstractController
         $price = $data['price'] ?? null;
         $description = $data['description'] ?? null;
         $quantity = $data['quantity'] ?? null;
+        $category = $data['category'] ?? null;
 
         if (!empty($name)) {
             $product->setName($name);
@@ -128,6 +144,9 @@ class ProductController extends AbstractController
         }
         if (!empty($quantity)) {
             $product->setQuantity($quantity);
+        }
+        if (!empty($category)) {
+            $product->setCategory($this->entityManager->getRepository(Category::class)->find($category->getId()));
         }
 
         return $product;
