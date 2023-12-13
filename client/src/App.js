8c438bfa6e15737fff88a664f76deb3eaac428ba
adrienrov <adrienrov@gmail.com>
2023-12-13 11:43:52 +0100

    Ajout nelmio & category

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from "react";
+import React, { useEffect, useState, useContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,7 +9,7 @@ import {
   Navigate,
   Routes
 } from "react-router-dom";
-
+import axios from 'axios';
 import Accueil from "./pages/accueil/Accueil";
 import Shopping from "./pages/shopping/Shopping";
 import Produit from "./pages/produit/Produit";
@@ -17,8 +17,24 @@ import Panier from "./pages/panier/Panier";
 import Contact from "./pages/contact/Contact";
 import NavBar from "./pages/commons/NavBar";
 import Footer from "./pages/commons/Footer";
+import Categories from "./pages/categories/Categories";
 
 function App() {
+
+  const url = "http://127.0.0.1:8000/categories";
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+      axios.get(url)
+          .then((response) => {
+            console.log(response.data);
+              setCategories(response.data);
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+  }, []);
+
   return (
     <Router>
       <NavBar />
@@ -29,6 +45,15 @@ function App() {
           <Route path="/produit/:id" element={<Produit />} />
           <Route path="/panier" element={<Panier />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<Accueil />} />
+          {categories.map(category => (
+            <Route
+              key={category.id}
+              path={`/categories/${category.id}`}
+              element={ <Categories title={category.name} />}
+            />
+          ))}
         </Routes>
       </div>
       <Footer />
