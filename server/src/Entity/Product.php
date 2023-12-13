8a1cac061f0b8307a5910bd8ab35b682f9a087d3
LEGRAND Hugo <legrand.hugo1732@gmail.com>
2023-12-13 09:59:11 +0100

    Finition du ProductController

--- server/src/Entity/Product.php
+++ server/src/Entity/Product.php
@@ -32,7 +32,7 @@ class Product
     private Collection $images;
 
     #[ORM\ManyToOne(inversedBy: 'products')]
-    private ?category $category = null;
+    private ?Category $category = null;
 
     public function __construct()
     {
@@ -122,12 +122,12 @@ class Product
         return $this;
     }
 
-    public function getCategory(): ?category
+    public function getCategory(): ?Category
     {
         return $this->category;
     }
 
-    public function setCategory(?category $category): static
+    public function setCategory(?Category $category): static
     {
         $this->category = $category;
 
