37538cd670ea6e17e1b9a3189f793d23b990c45d
Ricosmos <85259600+Ricosmos@users.noreply.github.com>
2023-12-13 15:54:56 +0100

    Contact page

--- client/src/pages/commons/Footer.jsx
+++ client/src/pages/commons/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="footer footer-center p-10 text-base-content rounded">
             <nav className="grid grid-flow-col gap-4">
                 <a className="link link-hover">A Propos</a>
-                <a className="link link-hover">Contact</a>
+                <a className="link link-hover" href="/Contact">Contact</a>
                 <a className="link link-hover">Shopping</a>
             </nav>
             <nav>
