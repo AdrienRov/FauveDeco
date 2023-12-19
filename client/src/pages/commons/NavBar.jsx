7fe1075b29019fd1a1df9a791a748a9867ca412b
Ricosmos <85259600+Ricosmos@users.noreply.github.com>
2023-12-19 11:34:06 +0100

    Conversion noir en blanc des dividers

--- client/src/pages/commons/NavBar.jsx
+++ client/src/pages/commons/NavBar.jsx
@@ -9,108 +9,106 @@ import Inscription from "../../form/Inscription";
 
 function NavBar(props) {
 	const [visible, setVisible] = useState(false);
-	const [form, setForm] = useState(<Connexion  />);
+	const [form, setForm] = useState(<Connexion />);
 	const [formKey, setFormKey] = useState(10);
 
-    const loggedIn = localStorage.getItem("user") !== null;
+	const loggedIn = localStorage.getItem("user") !== null;
 
 	const handleCallback = (data) => {
-        setVisible(data);
-        setFormKey(formKey + 1);
-    };
-    const handleConnexion = () => {
-        setForm(<Connexion parentCallback={handleCallback} handleSwitch={handleInscription} />);
-        setVisible(true);
-        setFormKey(formKey + 1);
-    }
-    const handleInscription = () => {
-        console.log("Inscription");
-        setForm(<Inscription parentCallback={handleCallback} handleSwitch={handleConnexion} />);
-        setVisible(true);
-        setFormKey(formKey + 1);
-    };
-    return (
-        <div>
-            <Modal key={formKey} parentCallback={handleCallback} open={visible} form={form} title="Connexion" />
+		setVisible(data);
+		setFormKey(formKey + 1);
+	};
+	const handleConnexion = () => {
+		setForm(<Connexion parentCallback={handleCallback} handleSwitch={handleInscription} />);
+		setVisible(true);
+		setFormKey(formKey + 1);
+	}
+	const handleInscription = () => {
+		console.log("Inscription");
+		setForm(<Inscription parentCallback={handleCallback} handleSwitch={handleConnexion} />);
+		setVisible(true);
+		setFormKey(formKey + 1);
+	};
+	return (
+		<div>
+			<Modal key={formKey} parentCallback={handleCallback} open={visible} form={form} title="Connexion" />
 
-            <div className="navbar">
-                <div className="navbar-start divider divider-neutral">
-                    <div className="dropdown px-15">
-                        <div tabIndex={0} role="button" className="btn btn-nav back-vert duration-150">Catégories<svg class="w-[15px] h-[15px] fill-[#ffffff]" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg"><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"></path></svg></div>
-                        <ul className="back-vert dropdown-content z-[1] menu shadow bg-base-100 w-60">
-                            <li><Link to="/categories/0" className="back-vert btn-nav dropdown-item text-black">
-                                Maison et objets
-                            </Link></li>
-                            <li><Link to="/categories/1" className="back-vert btn-nav dropdown-item text-black">
-                                Fragrances
-                            </Link></li>
-                            <li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
-                                Lifestyle
-                            </Link></li>
-                            <li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
-                                Décorations murales
-                            </Link></li>
-                            <li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
-                                Idées cadeaux
-                            </Link></li>
-                        </ul>
-                    </div>
-                    <Link to="https://www.instagram.com/fauve.lh/" className="mr-5">
-                        <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                        </svg>
-                    </Link>
-                    <Link to="https://www.facebook.com/profile.php?id=100094723031338">
-                        <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path>
-                        </svg>
-                    </Link>
-                </div>
+			<div className="navbar">
+				<div className="navbar-start divider divider-white">
+					<div className="dropdown px-15">
+						<div tabIndex={0} role="button" className="btn btn-nav back-vert duration-150">Catégories<svg class="w-[15px] h-[15px] fill-[#ffffff]" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg"><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"></path></svg></div>
+						<ul className="back-vert dropdown-content z-[1] menu shadow bg-base-100 w-60">
+							<li><Link to="/categories/0" className="back-vert btn-nav dropdown-item text-black">
+								Maison et objets
+							</Link></li>
+							<li><Link to="/categories/1" className="back-vert btn-nav dropdown-item text-black">
+								Fragrances
+							</Link></li>
+							<li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
+								Lifestyle
+							</Link></li>
+							<li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
+								Décorations murales
+							</Link></li>
+							<li><Link to="/categories/2" className="back-vert btn-nav dropdown-item text-black">
+								Idées cadeaux
+							</Link></li>
+						</ul>
+					</div>
+					<Link to="https://www.instagram.com/fauve.lh/" className="mr-5">
+						<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+							<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+						</svg>
+					</Link>
+					<Link to="https://www.facebook.com/profile.php?id=100094723031338">
+						<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+							<path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path>
+						</svg>
+					</Link>
+				</div>
 
-                <Link to="/accueil" className="navbar-center hidden lg:flex mx-5 mb-0">
-                    <p className="text-4xl text-white">fauve</p>
-                    <p className="text-white">DECORATION</p>
-                </Link>
+				<Link to="/accueil" className="navbar-center hidden lg:flex mx-5 mb-0">
+					<p className="text-4xl text-white">fauve</p>
+					<p className="text-white">DECORATION</p>
+				</Link>
 
-                <div className="navbar-end divider divider-neutral">
+				<div className="navbar-end divider divider-white">
+					{
+						loggedIn ? (
+							<Link to="/user" className="btn btn-ghost">
+								<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+									<path d="M224 288c70.7 0 128-57.3 128-128S294.7 32 224 32S96 89.3 96 160s57.3 128 128 128zm0 32c-88.4 0-176 43.52-176 128v32h352v-32c0-84.48-87.6-128-176-128zm224 128c0-53.02-43-96-96-96H96c-53 0-96 42.98-96 96v96c0 53 43 96 96 96h256c53 0 96-43 96-96v-96z"></path>
+								</svg>
+							</Link>
+						) : (
+							<Button className="btn btn-ghost" onClick={() => handleConnexion()}>
+								<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+									<path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"></path>
+								</svg>
+							</Button>
+						)
 
-                    
-                    {
-                        loggedIn ? (
-                            <Link to="/user" className="btn btn-ghost">
-                                <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M224 288c70.7 0 128-57.3 128-128S294.7 32 224 32S96 89.3 96 160s57.3 128 128 128zm0 32c-88.4 0-176 43.52-176 128v32h352v-32c0-84.48-87.6-128-176-128zm224 128c0-53.02-43-96-96-96H96c-53 0-96 42.98-96 96v96c0 53 43 96 96 96h256c53 0 96-43 96-96v-96z"></path>
-                                </svg>
-                            </Link>
-                                ) : (
-                            <Button className="btn btn-ghost" onClick={() => handleConnexion()}>
-                            <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"></path>
-                            </svg>
-                            </Button>
-                                )
+					}
 
-                    }
-
-                    <Link to="/shopping" className="btn btn-ghost">
-                        <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
-                    </Link>
-                    <Link to="/panier" className="btn btn-ghost">
-                        <div class="relative flex items-center justify-center">
-                        <svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
-                        {
-                            props.cart.length > 0 && (
-                                <div className="absolute top-0 right-0 w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs">
-                                    {props.cart.length}
-                                </div>
-                            )
-                        }
-                        </div>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
+					<Link to="/shopping" className="btn btn-ghost">
+						<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
+					</Link>
+					<Link to="/panier" className="btn btn-ghost">
+						<div class="relative flex items-center justify-center">
+							<svg class="w-[25px] h-[25px] fill-[#ffffff]" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+							{
+								props.cart.length > 0 && (
+									<div className="absolute top-0 right-0 w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-xs">
+										{props.cart.length}
+									</div>
+								)
+							}
+						</div>
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default NavBar;
