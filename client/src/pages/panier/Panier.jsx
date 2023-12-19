6784da72aefd404750438a8e653a5ae84d805925
Gauthier Joly <joly.gauthier76@gmail.com>
2023-12-19 11:15:03 +0100

    yes panier + carousel

--- client/src/pages/panier/Panier.jsx
+++ client/src/pages/panier/Panier.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
 import * as ReactDom from "react-dom";
-
 function Panier(props) {
 
     const { cart, setCart } = props;
+    const [step, setStep] = useState(1);
+
+    const user = JSON.parse(localStorage.getItem('user'));
 
     const fixPrice = (price) => {
         if (typeof price !== "number") {
@@ -13,6 +16,8 @@ function Panier(props) {
     }
 
     const removeFromCart = (produit) => {
+        if (step != 1)
+            return;
         const exist = cart.find((x) => x.id === produit.id);
         if (exist) {
             setCart(cart.filter((x) => x.id !== produit.id));
@@ -20,6 +25,11 @@ function Panier(props) {
     }
 
     const changeQuantity = (produit, quantity) => {
+        if (step != 1)
+            return;
+        if (typeof quantity !== "number") {
+            quantity = parseInt(quantity);
+        }
         if (quantity < 1) {
             return;
         }
@@ -33,42 +43,46 @@ function Panier(props) {
         }
     }
 
-    return (
-        <div className="py-7">
-            <h1 class="mb-10 text-center text-2xl font-bold">Panier</h1>
-            <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-                
-                <div class="rounded-lg md:w-2/3">
-                {cart.map((produit) => (
-                    <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                        <img src={produit.images[0].url} alt="product-image" class="w-full rounded-lg sm:w-40" />
-                        <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                            <div class="mt-5 sm:mt-0">
-                                <h2 class="text-lg font-bold text-gray-900">{produit.name}</h2>
-                                <p class="mt-1 text-xs text-gray-700">{fixPrice(produit.price)}€</p>
-                                <p class="mt-1 text-xs text-gray-700">{produit.category.name}</p>
-                            </div>
-                            <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                                <div class="flex items-center border-gray-100">
-                                    <button class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => changeQuantity(produit, produit.buy_quantity - 1)}> - </button>
-                                    <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={produit.buy_quantity} min="1" max={produit.quantity} onChange={(e) => changeQuantity(produit, e.target.value)} />
-                                    <button class="cursor-pointer rounded-r bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => changeQuantity(produit, produit.buy_quantity + 1)}> + </button>
-                                </div>
-                                <div class="flex items-center space-x-4">
-                                    <p class="text-sm">{fixPrice(produit.buy_quantity * produit.price)}</p>
-                                    <button class="text-red-500 hover:text-red-600" onClick={() => removeFromCart(produit)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>))}
-                
-                </div>
 
-                <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+    const SidePanel = () => {
+        
+        const [firstName, setFirstName] = useState(user?.firstName ?? "");
+        const [lastName, setLastName] = useState(user?.lastName ?? "");
+        const [address, setAddress] = useState(user?.address ?? "");
+        const [country, setCountry] = useState(user?.country ?? "");
+        const [typeLivraison, setTypeLivraison] = useState("click-collect");
+
+        const verifyStep = () => {
+            switch (step) {
+                case 1:
+                    if (cart.length < 1)
+                        return false;
+                    if (cart.some((produit) => produit.buy_quantity > produit.quantity))
+                        return false;
+                    return true;
+                    break;
+                case 2:
+                    if (firstName.length < 2)
+                        return false;
+                    if (lastName.length < 2)
+                        return false;
+                    if (address.length < 2)
+                        return false;
+                    if (country.length < 2)
+                        return false;
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+
+
+        switch (step) {
+            case 1:
+                // Total et boutton confirmer
+                return (<>
                     <div class="mb-2 flex justify-between">
                         <p class="text-gray-700">Total</p>
                         <p class="text-gray-700">{fixPrice(cart.reduce((a, c) => a + c.buy_quantity * c.price, 0))}€</p>
@@ -85,7 +99,97 @@ function Panier(props) {
                             <p class="text-sm text-gray-700">TVA incluse</p>
                         </div>
                     </div>
-                    <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Confirmer la commande</button>
+                    <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600" onClick={() => setStep(2)} disabled={!verifyStep()}>Confirmer la commande</button>
+                    </>)
+
+                break;
+            case 2:
+                    // Adresse de livraison
+                    // Fields: firstName, lastName, address, country
+
+                    return (<>
+
+                    <div class="mb-2 flex justify-between">
+                        <p class="text-gray-700">Adresse de livraison</p>
+                    </div>
+
+                    <input type="text" placeholder="Prénom" class="my-2 w-full rounded-md bg-gray-100 py-1.5 px-2 font-medium text-gray-700 hover:bg-gray-200" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    <input type="text" placeholder="Nom" class="my-2 w-full rounded-md bg-gray-100 py-1.5 px-2 font-medium text-gray-700 hover:bg-gray-200" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    <input type="text" placeholder="Adresse" class="my-2 w-full rounded-md bg-gray-100 py-1.5 px-2 font-medium text-gray-700 hover:bg-gray-200" value={address} onChange={(e) => setAddress(e.target.value)} />
+                    <input type="text" placeholder="Pays" class="my-2 w-full rounded-md bg-gray-100 py-1.5 px-2 font-medium text-gray-700 hover:bg-gray-200" value={country} onChange={(e) => setCountry(e.target.value)} />
+
+                    <hr class="my-4" />
+
+                    <div class="mb-2 flex justify-between">
+                        <p class="text-gray-700">Type de livraison</p>
+                    </div>
+
+                    <div class="flex">
+                        <input type="radio" class="mx-4" id="click-collect" name="type-livraison" value="click-collect" {...typeLivraison === "click-collect" && "checked"} onChange={(e) => setTypeLivraison(e.target.value)} />
+                        <label for="click-collect">Click and collect</label>
+                    </div>
+
+                    <div class="flex">
+                        <input type="radio" class="mx-4" id="livraison-domicile" name="type-livraison" value="livraison-domicile" {...typeLivraison === "livraison-domicile" && "checked"} onChange={(e) => setTypeLivraison(e.target.value)} />
+                        <label for="livraison-domicile">Livraison à domicile</label>
+                    </div>
+
+
+
+
+                    <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600" onClick={() => setStep(3)} disabled={!verifyStep()}>Continuer</button>
+
+                    </>)
+                    
+
+
+            default:
+                return (
+                    <>Erreur</>
+                )
+                break;
+        }
+    }
+
+    return (
+        <div className="py-7">
+            <h1 class="mb-10 text-center text-2xl font-bold">Panier</h1>
+            <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+                
+                <div class="rounded-lg md:w-2/3">
+                {cart.map((produit) => (
+                    <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                        <img src={produit.images[0].url} alt="product-image" class="w-full rounded-lg sm:w-40" />
+                        <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                            <Link to={`/produit/${produit.id}`}>
+                                <div class="mt-5 sm:mt-0">
+                                    <h2 class="text-lg font-bold text-gray-900">{produit.name}</h2>
+                                    <p class="mt-1 text-xs text-gray-700">{fixPrice(produit.price)}€</p>
+                                    <p class="mt-1 text-xs text-gray-700">{produit.category.name}</p>
+                                </div>
+                            </Link>
+                            <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                <div class="flex items-center border-gray-100">
+                                    <button class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => changeQuantity(produit, produit.buy_quantity - 1)}> - </button>
+                                    <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={produit.buy_quantity} min="1" max={produit.quantity} onChange={(e) => changeQuantity(produit, e.target.value)} />
+                                    <button class="cursor-pointer rounded-r bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => changeQuantity(produit, produit.buy_quantity + 1)}> + </button>
+                                </div>
+                                <div class="flex items-center space-x-4">
+                                    <p class="text-sm">{fixPrice(produit.buy_quantity * produit.price)}€</p>
+                                    <button class="text-red-500 hover:text-red-600" onClick={() => removeFromCart(produit)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>))}
+                
+                </div>
+
+                <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                    <SidePanel />
                 </div>
             </div>
         </div>
--- client/src/pages/produit/Produit.jsx
+++ client/src/pages/produit/Produit.jsx
@@ -22,6 +22,21 @@ function Produit(props) {
         });
     }, [id]);
 
+    // Carousel
+    const [currentImage, setCurrentImage] = useState(0);
+
+    // change every 5 seconds
+    useEffect(() => {
+        if (!produit?.images?.length) {
+            return;
+        }
+        const interval = setInterval(() => {
+            setCurrentImage((currentImage + 1) % produit.images.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [currentImage, produit?.images?.length]);
+
+
     if (!produit) {
         return (<div>Produit introuvable</div>);
     }
@@ -59,6 +74,10 @@ function Produit(props) {
         }
     }
 
+
+
+    
+
     const cartItem = cart.find((x) => x.id === produit.id);
     return (
         <div class="py-6">
@@ -68,9 +87,25 @@ function Produit(props) {
                         <div x-data="{ image: 1 }" x-cloak>
                             <div class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4">
                                 <div class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
-                                    <img className="object-cover w-full h-full rounded-lg" src={produit.images[0].url} alt="" />
+                                    {
+                                        produit.images.map((image, index) => (
+                                            <img src={image.url} alt="product" class={`h-full w-full object-cover ${index === currentImage ? 'block' : 'hidden'}`} />
+                                        ))
+                                    }                                 
                                 </div>
                             </div>
+                        </div>
+                        {
+                            // image thumbnails, when clicked, set the index as the current image
+                        }
+                        <div class="flex -mx-2 mb-4 justify-center">
+                            {
+                                produit.images.map((image, index) => (
+                                    <div class="h-12 md:h-16 rounded-lg bg-gray-100 overflow-hidden cursor-pointer mx-2" onClick={() => setCurrentImage(index)}>
+                                        <img src={image.url} alt="product" class="h-full w-full object-cover" />
+                                    </div>
+                                ))
+                            }
                         </div>
                         <div class="md:flex-1 px-4">
                             <h2 class="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">{ produit.name }</h2>
