8ba62765a8b6492b0daf8797b392e2a809381768
Gauthier Joly <joly.gauthier76@gmail.com>
2023-12-21 14:07:39 +0100

    user profile edit

--- client/src/pages/user/UserInfoField.jsx
+++ client/src/pages/user/UserInfoField.jsx
@@ -1,18 +1,27 @@
 // UserInfoField.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
-const UserInfoField = ({ label, value }) => (
-  <div className="mb-4 flex-1">
-    <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
-    <input
-      type="text"
-      name={label.toLowerCase()}
-      className="w-full px-3 py-2 border rounded-md"
-      disabled="disabled"
-      value={value}
-      autoComplete="off"
-    />
-  </div>
-);
+const UserInfoField = (porps) => {
+  
+  const [value, setValue] = useState(porps.value);
+
+  useEffect(() => {
+    setValue(porps.value);
+  }, [porps.value]);
+
+  return (<div className="mb-4 flex-1">
+      <label className="block text-gray-700 text-sm font-bold mb-2">{porps.label}</label>
+      <input
+        type="text"
+        name={porps.label.toLowerCase()}
+        className="w-full px-3 py-2 border rounded-md"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        autoComplete="off"
+      />
+    </div>
+  )
+};
 
 export default UserInfoField;
